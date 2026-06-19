54ae7d514564692691cbb392f8e1a8ba6edc2b36
richmanempire001-cyber <richmanempire001@gmail.com>
2026-06-19 18:21:56 -0400

    Create _app.js

---
+++ pages/_app.js
@@ -0,0 +1,31 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+
+export default function App({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      window.gtag('config', 'G-SBF43ZKBYK', { page_path: url });
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
+  return (
+    <>
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-SBF43ZKBYK" />
+      <script dangerouslySetInnerHTML={{
+        __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-SBF43ZKBYK');
+        `
+      }} />
+      <Component {...pageProps} />
+    </>
+  );
+}
