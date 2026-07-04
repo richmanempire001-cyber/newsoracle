703bfd96d42e1be23370352c16ad7774216c9d14
richmanempire001-cyber <richmanempire001@gmail.com>
2026-07-04 01:14:53 -0400

    Update sitemap.xml.js

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -9,21 +9,33 @@ function generateSiteMap(articles) {
   return `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url>
-    <loc>https://newsoracle.online</loc>
+    <loc>https://www.newsoracle.online</loc>
     <changefreq>hourly</changefreq>
     <priority>1.0</priority>
   </url>
   <url>
-    <loc>https://newsoracle.online/about</loc>
+    <loc>https://www.newsoracle.online/about</loc>
+    <changefreq>monthly</changefreq>
+    <priority>0.6</priority>
+  </url>
+  <url>
+    <loc>https://www.newsoracle.online/contact</loc>
+    <changefreq>monthly</changefreq>
     <priority>0.5</priority>
   </url>
   <url>
-    <loc>https://newsoracle.online/contact</loc>
-    <priority>0.5</priority>
+    <loc>https://www.newsoracle.online/privacy-policy</loc>
+    <changefreq>monthly</changefreq>
+    <priority>0.3</priority>
+  </url>
+  <url>
+    <loc>https://www.newsoracle.online/terms</loc>
+    <changefreq>monthly</changefreq>
+    <priority>0.3</priority>
   </url>
   ${articles.map(article => `
   <url>
-    <loc>https://newsoracle.online/article/${article.id}</loc>
+    <loc>https://www.newsoracle.online/article/${article.id}</loc>
     <lastmod>${new Date(article.created_at).toISOString()}</lastmod>
     <changefreq>never</changefreq>
     <priority>0.8</priority>
@@ -38,7 +50,6 @@ export async function getServerSideProps({ res }) {
     .order('created_at', { ascending: false });
 
   const sitemap = generateSiteMap(articles || []);
-
   res.setHeader('Content-Type', 'text/xml');
   res.write(sitemap);
   res.end();
