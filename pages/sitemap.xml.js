9ca52e0f5cac39cfd15d13076d5ab139d3299626
richmanempire001-cyber <richmanempire001@gmail.com>
2026-07-07 18:21:44 -0400

    Update sitemap.xml.js

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -1,17 +1,38 @@
 import { createClient } from '@supabase/supabase-js';
+import { slugify } from '../lib/slugify';
 
 const supabase = createClient(
   process.env.SUPABASE_URL,
   process.env.SUPABASE_KEY
 );
 
+function escapeXml(str) {
+  return (str || '').replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
+}
+
 function generateSiteMap(articles) {
   return `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+        xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">
   <url>
     <loc>https://www.newsoracle.online</loc>
     <changefreq>hourly</changefreq>
     <priority>1.0</priority>
+  </url>
+  <url>
+    <loc>https://www.newsoracle.online/category/sports</loc>
+    <changefreq>daily</changefreq>
+    <priority>0.9</priority>
+  </url>
+  <url>
+    <loc>https://www.newsoracle.online/category/finance</loc>
+    <changefreq>daily</changefreq>
+    <priority>0.9</priority>
+  </url>
+  <url>
+    <loc>https://www.newsoracle.online/category/politics</loc>
+    <changefreq>daily</changefreq>
+    <priority>0.9</priority>
   </url>
   <url>
     <loc>https://www.newsoracle.online/about</loc>
@@ -33,27 +54,35 @@ function generateSiteMap(articles) {
     <changefreq>monthly</changefreq>
     <priority>0.3</priority>
   </url>
-  ${articles.map(article => `
+  ${articles.map(article => {
+    const slug = `${article.id}-${slugify(article.title)}`;
+    const imageTag = article.image ? `
+    <image:image>
+      <image:loc>${escapeXml(article.image)}</image:loc>
+      <image:title>${escapeXml(article.title)}</image:title>
+    </image:image>` : '';
+    return `
   <url>
-    <loc>https://www.newsoracle.online/article/${article.id}</loc>
+    <loc>https://www.newsoracle.online/article/${slug}</loc>
     <lastmod>${new Date(article.created_at).toISOString()}</lastmod>
     <changefreq>never</changefreq>
-    <priority>0.8</priority>
-  </url>`).join('')}
+    <priority>0.8</priority>${imageTag}
+  </url>`;
+  }).join('')}
 </urlset>`;
 }
 
 export async function getServerSideProps({ res }) {
   const { data: articles } = await supabase
     .from('articles')
-    .select('id, created_at')
+    .select('id, created_at, title, image')
     .order('created_at', { ascending: false });
 
   const sitemap = generateSiteMap(articles || []);
   res.setHeader('Content-Type', 'text/xml');
+  res.setHeader('Cache-Control', 'public, s-maxage=3600, stale-while-revalidate=7200');
   res.write(sitemap);
   res.end();
-
   return { props: {} };
 }
 
